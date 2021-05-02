f36837748a98b4ace9ac0b54d090bdde5060f9a1
Laddge <dev.laddge@gmail.com>
2021-05-02 16:59:13 +0900

    add new badge

--- src/components/Top-info.vue
+++ src/components/Top-info.vue
@@ -2,6 +2,13 @@
     <div>
 
         <v-list-item :input-value="popupEnable" @click="popupEnable = true">
+            <v-chip
+                outlined
+                x-small
+                color="red"
+                v-if="formatedTime.isNew">
+                    NEW
+            </v-chip>
             {{ title }}
             <v-spacer></v-spacer>
             {{ formatedTime.mm }}/{{ formatedTime.dd }}
@@ -37,11 +44,17 @@ export default {
   computed: {
     formatedTime: function () {
       var d = new Date(this.time);
+      var now = new Date();
+      var diff = Math.abs((now.getTime() - d.getTime()));
+      var isNew = false;
+      if (diff <= 3600000) {
+        isNew = true;
+      }
       var mm = d.getMonth() + 1;
       var dd = d.getDate();
       var hh = ('0' + d.getHours()).slice(-2);
       var mi = ('0' + d.getMinutes()).slice(-2);
-      return {'mm': mm, 'dd': dd, 'hh': hh, 'mi': mi}
+      return {'mm': mm, 'dd': dd, 'hh': hh, 'mi': mi, 'isNew': isNew}
     }
   },
   data: () => ({
