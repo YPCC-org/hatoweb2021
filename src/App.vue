020bf17dc5f31850052ec5935dd54337a31a0e83
Laddge <dev.laddge@gmail.com>
2021-05-07 16:58:04 +0900

    [feat] タイトルを追加した。

--- src/App.vue
+++ src/App.vue
@@ -61,8 +61,37 @@ export default {
     components: {
     },
 
+    mounted : function(){
+        let routeInstance = this.$route;
+        this.createTitleDesc(routeInstance);
+    },
+
     data: () => ({
         drawer: false,
     }),
+
+    methods: {
+        createTitleDesc : function(routeInstance){
+            if(routeInstance.meta.title){
+                let setTitle = routeInstance.meta.title;
+                document.title = setTitle;
+            } else {
+                document.title = 'title is not set'
+            }
+            if(routeInstance.meta.desc){
+                let setDesc = routeInstance.meta.desc + ' | MIYACHIN VUE';
+                document.querySelector("meta[name='description']").setAttribute('content', setDesc)
+            } else {
+                document.querySelector("meta[name='description']").setAttribute('content', 'description is not set')
+            }
+        },
+    },
+
+    watch: {
+        '$route' (routeInstance, from) {
+            from;
+            this.createTitleDesc(routeInstance);
+        }
+    },
 };
 </script>
