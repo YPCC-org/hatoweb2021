426e33b00f762e86092167a5e0165793c1b875f1
Laddge <dev.laddge@gmail.com>
2021-06-28 18:41:38 +0900

    [feat] 二日目、三日目のスケジュールを入力

--- src/components/schedules/Sad.vue
+++ src/components/schedules/Sad.vue
@@ -5,26 +5,61 @@
         </th>
         <tr>
             <td>9:00</td>
-            <td></td>
-            <td colspan=11 rowspan=2>
+            <td colspan=12 rowspan=1 class="pt-3 pl-5">
                 <v-card outlined>
-                    <p>8:40~</p>
-                    <p>クラス・係準備</p>
-                    <p>前夜祭クラスリハ</p>
+                    <p>8:30~ SHR・清掃・準備</p>
+                </v-card>
+                <v-card outlined class="mt-3">
+                    <p>9:30~</p>
+                    <p>学級展・模擬店・喫茶・ハトヤ 開始</p>
                 </v-card>
             </td>
         </tr>
         <tr>
             <td>10:00</td>
+            <td colspan=4 rowspan=6 class="pt-3 pl-5">
+                <v-card outlined height="300">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <p>学芸班発表</p>
+                    <p>(２体)</p>
+                </v-card>
+            </td>
+            <td colspan=4 rowspan=6 class="pt-3 pl-5">
+                <v-card outlined height="300">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <p>中庭</p>
+                </v-card>
+            </td>
         </tr>
         <tr>
             <td>11:00</td>
+            <td colspan=4 rowspan=2 class="pt-3 pl-5">
+                <v-card outlined>
+                    <p>10:30~</p>
+                    <p>月見亭</p>
+                </v-card>
+            </td>
         </tr>
         <tr>
             <td>12:00</td>
         </tr>
         <tr>
             <td>13:00</td>
+            <td colspan=4 rowspan=2 class="pt-3 pl-5">
+                <v-card outlined>
+                    <p>12:30~</p>
+                    <p>理数科展</p>
+                </v-card>
+            </td>
         </tr>
         <tr>
             <td>14:00</td>
@@ -34,9 +69,21 @@
         </tr>
         <tr>
             <td>16:00</td>
+            <td colspan=12 rowspan=1 class="pt-3 pl-5">
+                <v-card outlined>
+                    <p>15:30~ 片付け</p>
+                </v-card>
+            </td>
         </tr>
         <tr>
             <td>17:00</td>
+            <td colspan=12 rowspan=2 class="pt-3 pl-5">
+                <v-card outlined>
+                    <p>16:30~</p>
+                    <p>中夜祭</p>
+                    <p>(グラウンド)</p>
+                </v-card>
+            </td>
         </tr>
         <tr>
             <td>18:00</td>
--- src/components/schedules/Sun.vue
+++ src/components/schedules/Sun.vue
@@ -5,23 +5,53 @@
         </th>
         <tr>
             <td>9:00</td>
-            <td></td>
-            <td colspan=11 rowspan=2>
+            <td colspan=12 rowspan=1 class="pt-3 pl-5">
                 <v-card outlined>
-                    <p>8:40~</p>
-                    <p>クラス・係準備</p>
-                    <p>前夜祭クラスリハ</p>
+                    <p>8:30~ SHR・清掃・準備</p>
+                </v-card>
+                <v-card outlined class="mt-3">
+                    <p>9:30~</p>
+                    <p>学級展・模擬店・喫茶・ハトヤ 開始</p>
                 </v-card>
             </td>
         </tr>
         <tr>
             <td>10:00</td>
+            <td colspan=4 rowspan=6 class="pt-3 pl-5">
+                <v-card outlined height="300">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <p>学芸班発表</p>
+                    <p>(２体)</p>
+                </v-card>
+            </td>
+            <td colspan=4 rowspan=6 class="pt-3 pl-5">
+                <v-card outlined height="300">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <p>中庭</p>
+                </v-card>
+            </td>
         </tr>
         <tr>
             <td>11:00</td>
         </tr>
         <tr>
             <td>12:00</td>
+            <td colspan=4 rowspan=3 class="pt-3 pl-5">
+                <v-card outlined height="150">
+                    <p>11:30~</p>
+                    <br>
+                    <p>MHK</p>
+                </v-card>
+            </td>
         </tr>
         <tr>
             <td>13:00</td>
@@ -34,9 +64,23 @@
         </tr>
         <tr>
             <td>16:00</td>
+            <td colspan=12 rowspan=1 class="pt-3 pl-5">
+                <v-card outlined>
+                    <p>15:30~</p>
+                    <p>片付け・会場整理・ファイヤーストーム準備</p>
+                </v-card>
+            </td>
         </tr>
         <tr>
             <td>17:00</td>
+            <td colspan=12 rowspan=3 class="pt-3 pl-5">
+                <v-card outlined height="150">
+                    <p>17:00~</p>
+                    <p>閉祭式</p>
+                    <p>後夜祭</p>
+                    <p>(グラウンド)</p>
+                </v-card>
+            </td>
         </tr>
         <tr>
             <td>18:00</td>
