feb145e6fad613a10eba05b8805a43e1b0dbb202
Laddge <dev.laddge@gmail.com>
2021-07-03 07:00:18 +0900

    [feat] スケジュールのデフォルトタブを2日目に

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -115,8 +115,8 @@ export default {
                         if (this.$route.query.tab != undefined) {
                             ctab = 'tab-' + this.$route.query.tab;
                         } else {
-                            this.$router.replace({ query: { tab: '1' } });
-                            ctab = 'tab-1';
+                            this.$router.replace({ query: { tab: '2' } });
+                            ctab = 'tab-2';
                         }
                     }
                     return ctab
