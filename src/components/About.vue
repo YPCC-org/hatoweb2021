16e74a12aadc9efbd7b648933493d72faf48a528
Laddge <dev.laddge@gmail.com>
2021-07-03 09:12:03 +0900

    [feat] アナリティクスの説明を追加

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,6 +7,10 @@
     <p>2年1組髙橋と2年4組加藤の二人で制作しました。</p>
     <p>様々な方の協力によってこのような形で実現できたことを嬉しく思います。</p>
     <p>ご協力ありがとうございます。</p>
+    <h3>情報収集について</h3>
+    <p>このサイトでは利用状況を分析するために Google アナリティクス を使用しています。</p>
+    <p>Google アナリティクスはCookieを使用してこのサイトへのアクセス情報を Google に送信しています。</p>
+    <p>Google アナリティクスでデータが収集、処理される仕組みについては「Google のサービスを使用するサイトやアプリから収集した情報の Google による使用」のページ（<a href="https://policies.google.com/technologies/partner-sites" target="_brank">https://policies.google.com/technologies/partner-sites</a>）をご覧ください。</p>
   </div>
 </template>
 
